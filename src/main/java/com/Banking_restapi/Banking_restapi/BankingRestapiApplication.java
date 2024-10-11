29653042d1e3dcb0e7ee61b8c35f8acf775a2713
Abhishek Bhosale <abhishekbhosale676@gmail.com>
2024-10-11 14:19:05 +0530

    Changes to be detected

--- src/main/java/com/Banking_restapi/Banking_restapi/BankingRestapiApplication.java
+++ src/main/java/com/Banking_restapi/Banking_restapi/BankingRestapiApplication.java
@@ -8,7 +8,7 @@ public class BankingRestapiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BankingRestapiApplication.class, args);
-		System.out.println("hello cloud counselage . Abhiii ");
+		System.out.println("hello cloud counselage modified ");
 	}
 
 }
